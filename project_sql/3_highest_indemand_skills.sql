0e6636492ffa02f2d646f9e66884a14aa0bbc8c0
phanimoo <jphan2016@gmail.com>
2024-07-12 18:38:46 -0700

    Read_Me_xls_csv_updates

--- project_sql/3_highest_indemand_skills.sql
+++ project_sql/3_highest_indemand_skills.sql
@@ -15,7 +15,8 @@ INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
 INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst' AND
-    job_location = 'Phoenix, AZ'
+    job_work_from_home = TRUE
+    -- job_location = 'Phoenix, AZ'
 GROUP BY
     skills
 ORDER BY
